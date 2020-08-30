cd333b593f3f0d1669b27a33663617ff14b7c7a4
Martin <office@martinpagels.de>
2020-08-30 14:42:01 +0200

    Install styled-components, add GlobalStyles.js
    Add & import GlobalStyles.js

---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+}
+
+body {
+    margin: 0;
+    font-size: 112.5%;
+}
+`
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import GlobalStyles from './GlobalStyles'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
